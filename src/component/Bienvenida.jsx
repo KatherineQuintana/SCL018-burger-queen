a3787420698d7df030d3f0ff44f82693b961562c
KatherineQuintana <katty.quintana@gmail.com>
2022-01-18 11:23:44 -0300

    instalación react-router-dom

---
+++ src/component/Bienvenida.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Bienvenida = () => {
+    const perfect = 'funciona perfecto';
+    return (
+        <div>
+            <p>probando los componentes, {perfect}</p>
+            <h5>{perfect}</h5>
+        </div>
+    )
+}
+
+export default Bienvenida
---
+++ src/component/prueba.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Prueba = () => {
+    return (
+        <div>
+            <p>hola</p>
+        </div>
+    )
+}
+
+export default Prueba
