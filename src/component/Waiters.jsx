a79b07234c78befc409d098c2d1c30a40fe0a7d5
KatherineQuintana <katty.quintana@gmail.com>
2022-01-24 15:33:09 -0300

    data imprimiendo

--- src/component/Waiters.jsx
+++ src/component/Waiters.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 
 
 const Waiters = () => {
     const perfect = 'Aqui se debe pedir';
+
+    const [order, setOrder] = useState(null);
+
+    useEffect(() =>{
+        console.log ('useEffect')
+        extractData()
+    },[])
+
+    const extractData = async () => {
+        const data = await fetch('./menu.json')
+        const dataUser = await data.json()
+        console.log(dataUser)
+        setOrder(dataUser)
+    }
 
 
     return (
@@ -11,6 +25,14 @@ const Waiters = () => {
             <p>{perfect}</p>
             <Link to="/Home" className='btn btn-outline-success'> Home </Link>
            <Link to="/Kitchen" className='btn btn-outline-success'> Cocina </Link>
+           <ul>
+               {
+                   
+                   order && order.bebCalientes.map(menu => (
+                       <li key={menu.id}> {menu.valor}</li>
+                   ))
+               }
+           </ul>
         </div>
     )
 }
