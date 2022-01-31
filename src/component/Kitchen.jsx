5e40e21c40253eb258ffeae87cbaacaa00111b5e
KatherineQuintana <katty.quintana@gmail.com>
2022-01-31 15:47:48 -0300

    nav responsive in waiters

--- src/component/Kitchen.jsx
+++ src/component/Kitchen.jsx
@@ -5,10 +5,20 @@ const Kitchen = () => {
     return (
         <div className='container mt-5'>
             <p>Aqui va la cocina</p>
-            <Link to="/Home" className='btn btn-outline-success'> Home </Link>
-           <Link to="/Waiters" className='btn btn-outline-success'> Pedidos </Link>
+            <Link to="/Home" className='btn btn-outline-primary'> Home </Link>
+           <Link to="/Waiters" className='btn btn-outline-primary'> Pedidos </Link>
+           <hr/>
+           
         </div>
     )
 }
 
 export default Kitchen
+
+/* {           
+    order && order.bebidas.map(menu => (
+        <><img id="imgMenu" key={menu.id} src={menu.img}></img>
+        <li> {menu.bebestible} {menu.valor} </li></>
+    ))
+} */
+
